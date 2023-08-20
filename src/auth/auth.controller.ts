f150d2f7b908b6e9202b16de5309c6a6db380c31
Rocio Baigorria <intzabai@gmail.com>
2023-08-20 14:57:58 -0300

    new commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,8 +10,12 @@ export class AuthController {
     ){}
 
     @Post('register')
-    register(){
-        return this.authService.register();
+    register(
+        @Body()
+        registerDto: RegisterDto,
+    ){
+        console.log(registerDto);
+        return this.authService.register(registerDto);
     }
 
     @Post('login')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -7,9 +9,19 @@ export class AuthService {
     constructor (private readonly usersService: UsersService
         ){}
 
-    register(){
-        return 'register';
+    async register({name,email,password}: RegisterDto){ 
+
+        const user = await this.usersService.findOneByEmail(email);
+        if(user){
+            throw new BadRequestException('User already exists');
+        }
+       return await this.usersService.create({
+        name,
+        email,
+        password: await bcryptjs.hash(password,10)
+    });
     }
+
 
     login(){
         return 'login';
